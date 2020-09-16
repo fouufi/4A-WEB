53c2342fb91164f37ed730955406a96eea18857d
fanny <fanny@morvan.me>
2020-09-16 14:31:12 +0200

    ajout commentaires

--- script1.js
+++ script1.js
@@ -1,16 +1,17 @@
 var http = require('http'); //http contient ttes les fonctions et objets exportés par le module
-var url = require('url');
-var querystring = require('querystring');
+var url = require('url'); //ajout du module url
+var querystring = require('querystring'); //ajout du module querystring
 
-var serveur = http.createServer(function(req, res)
+var serveur = http.createServer(function(req, res) //Callback
 {
-    console.log(req.url);
-    var requete = url.parse(req.url);
-    var chemin = requete.pathname;
-    var params = querystring.parse(requete.query);
-
+    console.log(req.url); //Affichage de la requete
+    var requete = url.parse(req.url); //Requete
+    var chemin = requete.pathname; //Récupération du chemin de la requete
+    var params = querystring.parse(requete.query); //Récupération des arguments de la requete
+    var ip = req.connection.remoteAddress; //Récupération de l'addresse ip du client
     res.writeHead(200, {"Content-type": "text/plain"});
     
+    //Dire bonjour 
     if(chemin == "/direBonjour")
     {
         if ((("nom" in params)== false) ||("prenom" in params) == false)
@@ -19,6 +20,8 @@ var serveur = http.createServer(function(req, res)
         }
         res.end("Salut " + params.nom + " " + params.prenom + "!");
     }
+
+    //Dire au revoir
     else if (chemin == "/direAuRevoir")
     {
         if ((("nom" in params)== false) ||("prenom" in params) == false)
@@ -27,6 +30,8 @@ var serveur = http.createServer(function(req, res)
         }
         res.end("Au Revoir " + params.nom + " " + params.prenom + "!");
     }
+
+    //Erreur car pas de reconnaissance de la requete
     else
     {
         res.end("Service inconnu");
