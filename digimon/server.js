ac46666d116c508e843aad027975b37de66d2ecd
fanny <fanny@morvan.me>
2020-12-30 16:22:57 +0100

    first try create session

--- digimon/server.js
+++ digimon/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var serve_static = require('serve-static');
+const session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var http = require('http');
 var fs = require('fs');
+var cookieParser = require('cookie-parser');
 
 console.log("Chargement de la base de données");
 let database = JSON.parse(fs.readFileSync('pokedex.json')); //Lecture de la db
@@ -10,6 +13,38 @@ var app = express();
 //Activation du serveur statique
 app.use(serve_static(__dirname+"/public"));
 
+// Populate req.cookies
+app.use(cookieParser());
+
+// Session setup
+app.use(session({
+    store: new FileStore({
+ 
+        path: './session-store'
+
+    }),
+    name:'little cookie',
+    secret: 'This is very secret :3',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        // five year cookie
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 5
+    }
+}));
+
+app.get('/', function (req, res) {
+ 
+    // simple count for the session
+    if (!req.session.count) {
+        req.session.count = 0;
+    }
+    req.session.count += 1;
+ 
+    // respond with the session object
+    res.json(req.session);
+ 
+});
 app.get('/pokemonsParCat/:categorie', function (req, res) {
     if (req.params.categorie=='all') res.send({statut:'OK', pokemons:database.pokemons});
     else{
@@ -58,6 +93,10 @@ app.get('/pokemons', function (req, res) {
 
 app.get('/categories', function (req, res) {
 	res.send(database.types);
+});
+
+app.get('/favoris', function (req, res) {
+	res.send(sess.favoris);
 });
 
   
